b850b71313790015f320c75f51165a7a995c6e11
PRANITHA P <pranitha0174@gmail.com>
2026-09-21 23:01:20 +0530

    Add LeetCode solution: jump-game (java)

---
+++ LeetCode/jump-game/Solution.java
@@ -0,0 +1,20 @@
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int maxReach = 0;
+4        
+5        for (int i = 0; i < nums.length; i++) {
+6            if (i > maxReach) {
+7                return false;
+8            }
+9            
+10            maxReach = Math.max(maxReach, i + nums[i]);
+11            
+12            if (maxReach >= nums.length - 1) {
+13                return true;
+14            }
+15        }
+16        
+17        return true;
+18    }
+19}
+20
