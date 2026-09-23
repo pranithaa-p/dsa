404d6a12a05029d6019e733b9b7826fd77ba2bf6
PRANITHA P <pranitha0174@gmail.com>
2026-09-23 23:05:12 +0530

    Add LeetCode solution: plus-one (java)

---
+++ LeetCode/plus-one/Solution.java
@@ -0,0 +1,16 @@
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        for (int i = digits.length - 1; i >= 0; i--) {
+4            if (digits[i] < 9) {
+5                digits[i]++;
+6                return digits;
+7            }
+8            digits[i] = 0;
+9        }
+10        
+11        int[] newDigits = new int[digits.length + 1];
+12        newDigits[0] = 1;
+13        return newDigits;
+14    }
+15}
+16
